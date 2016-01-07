147f94ca062e45349aa3619a9f3326924be398f2
Agung Surya <agung.surya@gajahmerahterbang.com>
2016-01-07 08:49:04 +0700

    Merge

--- afterlog/index.php
+++ afterlog/index.php
@@ -3,7 +3,7 @@
     <?php
     session_start();
     if (!isset($_SESSION['username'])) {
-        header("location:../index.html");
+        //header("location:../index.html");
     }
     ?>   
     <head>
@@ -11,7 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>DESAIN WEB</title>
+        <title>HEART DESAIN WEB</title>
         <link href="../img/logo/logo1.png" rel="shortcut icon" />
 
         <!-- Bootstrap core CSS -->
@@ -21,9 +21,11 @@
         <link href="../css/main.css" rel="stylesheet">
         <link href="../css/font-awesome.min.css" rel="stylesheet">
         <link href="../css/animate-custom.css" rel="stylesheet">
-        <link href="../css/plugin/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/plugin/app.css" rel="stylesheet" type="text/css"/>
-        
+        <link src="../fonts/1942.ttf" rel="fonts">
+        <link href="../css/quiz.css" rel='stylesheet' type='text/css' />
+        <link href="../css/plugin/app.css" rel='stylesheet' type='text/css' />
+
+
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
         <!-- js -->
@@ -62,10 +64,19 @@
         
         <!-- ==== KUIS 1 ==== -->
         <?PHP include 'section/kuis1.html' ?>
-        <?PHP include 'section/kuis2.html' ?>
         <!-- container --> 
-		<?PHP include 'section/footer.html' ?>
         
+        <!-- ==== KUIS 2==== -->
+        <!-- container --> 
+        
+        <!-- ==== KUIS 3==== -->
+        <!-- container --> 
+        
+        <!-- ==== KUIS 4==== -->
+        <!-- container --> 
+
+        <!--footer-->
+        <?PHP include 'section/footer.html' ?>
         <!--end footer-->
  <a href="#0" class="cd-top">Top</a>
         <!--/login-->  
@@ -73,14 +84,16 @@
             ================================================== --> 
         <!-- Placed at the end of the document so the pages load faster --> 
         <script src="../js/jquery.min.js"></script>
-        <!--<script type="text/javascript" src=../js/modernizr.custom.js"></script>-->
+        <script type="text/javascript" src="../js/modernizr.custom.js"></script>
+        <script type="text/javascript" src="../js/modernizr.custom.js"></script>
         <script type="text/javascript" src="../js/bootstrap.min.js"></script> 
         <script type="text/javascript" src="../js/retina.js"></script> 
         <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script> 
         <script type="text/javascript" src="../js/smoothscroll.js"></script> 
         <script type="text/javascript" src="../js/jquery-func.js"></script>
-        <script type="text/javascript" src="../js/main.js"></script>
-        <script type="text/javascript" src="../js/modernizr.js"></script>
+        <!--<script type="text/javascript" src="../js/kroggy-v0.1.js"></script> Script ini konflik dengan AngularJS-->
+        <script src="../js/main.js" type="text/javascript"></script>
+        <script src="../js/modernizr.js" type="text/javascript"></script>
         
         <script type="text/javascript">
             $(function () {
@@ -89,6 +102,7 @@
                 });
             });
         </script>
+        
         <!-- Ini untuk js-nya plugin ya dek -->
         <script type="text/javascript" src="../js/plugin/libs/angular.min.js"></script>
         <script type="text/javascript" src="../js/plugin/libs/angular-animate.min.js"></script>
--- js/plugin/app.js
+++ js/plugin/app.js
@@ -1,103 +1,108 @@
 var app = angular.module('alfi', ['ngAnimate', 'ng-fx']);
 
-app.run(['$rootScope',
-    function ($rootScope) {
-        $rootScope.init = function () {
-            $rootScope.displayW = 900; // Gunakan ini untuk set lebar dari tampilan utama aplikasi secara global
-            $rootScope.started = true;
-            $rootScope.begin = false;
-            $rootScope.ended = false;
-        };
-        $rootScope.init();
-    }]);
+app.directive('quiz', [
+    function () {
+        return {
+            restrict: 'A',
+            scope: true,
+            templateUrl: 'section/kuisTemplate.html',
+            bindToController: { quiz: '@' },
+            controllerAs: 'quiz',
+            controller: ['$scope', '$http', '$timeout',
+                function ($scope, $http, $timeout) {
+                    var vm = this;
+                    vm.init = function () {
+                        vm.displayW = 900; // Gunakan ini untuk set lebar dari tampilan utama aplikasi secara global
+                        vm.started = true;
+                        vm.begin = false;
+                        vm.ended = false;
+                    };
+                    vm.init();
 
-app.controller('mainCtrl', ['$rootScope', '$scope', '$http', '$timeout',
-    function ($rootScope, $scope, $http, $timeout) {
-        $scope.reset = function () {
-            $scope.questions = [];
-            $scope.answers = [];
-            $scope.answered = 0;
-            $scope.position = 0;
-            $scope.submitReady = false;
-        }
+                    vm.reset = function () {
+                        vm.questions = [];
+                        vm.answers = [];
+                        vm.answered = 0;
+                        vm.position = 0;
+                        vm.submitReady = false;
+                    }
 
-        $scope.run = function () {
+                    vm.run = function () {
 
-            $http.get('kuis1/questions.php').success(function (response) {
-                $scope.questions = response.result;
-                console.log($scope.questions);
-                angular.forEach(response.result, function (value, key) {
-                    $scope.answers.push({ 'id': value.id, 'answer': 0 });
-                });
-            });
-            
-            $scope.startApp = function () {
-                var s = $timeout(function () {
-                    $rootScope.started = false;
-                    $rootScope.begin = true;
-                    $timeout.cancel(s);
-                });
-            }
-            $scope.submit = function () {
-                if ($scope.position === $scope.questions.length - 1) {
-                    console.log('submit');
-                    $http.post('kuis1/submit.php', $scope.answers).success(function (response) {
-                        $scope.results = response.result;
-                        console.log(JSON.stringify($scope.results));
-                        var correct = 0;
-                        angular.forEach($scope.results, function (value) {
-                            if (value.result) {
-                                console.log(value.result);
-                                correct++;
-                            }
+                        $http.get('questions.php').success(function (response) { // Change the endpoint as you wish
+                            vm.questions = response.result;
+                            angular.forEach(response.result, function (value, key) {
+                                vm.answers.push({ 'id': value.id, 'answer': 0 });
+                            });
                         });
-                        $scope.score = Math.round(((correct / $scope.results.length) * 100) * 100) / 100;
-                        var distance = (900 * $scope.results.length - 1);
-                        console.log($scope.results.length);
-                        console.log(distance);
-                        $rootScope.ended = true;
-                        $('.scrolls').animate({ scrollLeft: distance }, 500);
-                        console.log($scope.results);
-                    });
-                }
-            }
-            $scope.next = function () {
-                if ($scope.position < $scope.questions.length - 1) {
-                    var distance = 900 + (900 * $scope.position);
-                    console.log(distance);
-                    $('.scrolls').animate({ scrollLeft: distance }, 500);
-                    $scope.position++;
-                    if ($scope.position === $scope.questions.length - 1) {
-                        $scope.submitReady = true;
-                    }
-                }
-            }
-            $scope.prev = function () {
-                if ($scope.position > 0) {
-                    $scope.submitReady = false;
-                    var distance = (900 * ($scope.position)) - 900;
-                    console.log(distance);
-                    $('.scrolls').animate({ scrollLeft: distance }, 500);
-                    $scope.position--;
-                    if ($scope.position === $scope.questions.length - 2) {
-                        $scope.submitReady = false;
-                    }
-                }
-            }
-            $scope.choose = function (id, index, answerId) {
-                console.log($scope.answers[$scope.position].answer);
-                if ($scope.answers[$scope.position].answer == 0) { // If hasn't answered, iterate the scope.answered
-                    $scope.answered++;
-                }
-                $scope.answers[$scope.position] = { "id": id, "index": index, "answer": answerId };
-            }
+                        vm.startApp = function () {
+                            var s = $timeout(function () {
+                                vm.started = false;
+                                vm.begin = true;
+                                $timeout.cancel(s);
+                            });
+                        }
+                        vm.submit = function () {
+                            if (vm.position === vm.questions.length - 1) {
+                                $http.post('submit.php', vm.answers).success(function (response) {
+                                    vm.results = response.result;
+                                    console.log(JSON.stringify(vm.results));
+                                    var correct = 0;
+                                    angular.forEach(vm.results, function (value) {
+                                        if (value.result) {
+                                            console.log(value.result);
+                                            correct++;
+                                        }
+                                    });
+                                    vm.score = Math.round(((correct / vm.results.length) * 100) * 100) / 100;
+                                    var distance = (900 * vm.results.length - 1);
+                                    console.log(vm.results.length);
+                                    console.log(distance);
+                                    vm.ended = true;
+                                    angular.element('#quiz-' + vm.quiz + '.scrolls').animate({ scrollLeft: distance }, 500);
+                                    console.log(vm.results);
+                                });
+                            }
+                        }
+                        vm.next = function () {
+                            if (vm.position < vm.questions.length - 1) {
+                                var distance = 900 + (900 * vm.position);
+                                console.log(distance);
+                                angular.element('#quiz-' + vm.quiz + '.scrolls').animate({ scrollLeft: distance }, 500);
+                                vm.position++;
+                                if (vm.position === vm.questions.length - 1) {
+                                    vm.submitReady = true;
+                                }
+                            }
+                        }
+                        vm.prev = function () {
+                            if (vm.position > 0) {
+                                vm.submitReady = false;
+                                var distance = (900 * (vm.position)) - 900;
+                                console.log(distance);
+                                angular.element('#quiz-' + vm.quiz + '.scrolls').animate({ scrollLeft: distance }, 500);
+                                vm.position--;
+                                if (vm.position === vm.questions.length - 2) {
+                                    vm.submitReady = false;
+                                }
+                            }
+                        }
+                        vm.choose = function (id, index, answerId) {
+                            console.log(vm.answers[vm.position].answer);
+                            if (vm.answers[vm.position].answer == 0) { // If hasn't answered, iterate the vm.answered
+                                vm.answered++;
+                            }
+                            vm.answers[vm.position] = { "id": id, "index": index, "answer": answerId };
+                        }
 
-            $scope.reload = function () {
-                $rootScope.init();
-                $scope.reset();
-                $scope.run();
-            }
+                        vm.reload = function () {
+                            vm.init();
+                            vm.reset();
+                            vm.run();
+                        }
+                    }
+                    vm.reset();
+                    vm.run();
+                }]
         }
-        $scope.reset();
-        $scope.run();
     }]);
